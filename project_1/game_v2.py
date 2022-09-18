e9b0510ecdeffb1df84e69bb03448ba014151aca
Rodiges24gone <sencerf@yandex.ru>
2022-09-18 16:48:05 +0300

    Новый проект

---
+++ project_1/game_v2.py
@@ -0,0 +1,58 @@
+"""Игра угадай число
+Компьютер сам загадывает и сам угадывает число
+"""
+
+from re import L
+import numpy as np
+
+
+def random_predict(number: int = 1) -> int:
+    """Рандомно угадываем число
+
+    Args:
+        number (int, optional): Загаданное число. Defaults to 1.
+
+    Returns:
+        int: Число попыток
+    """
+    count = 0
+    l = 1
+    r = 101
+    
+    while True:
+        count += 1
+        predict_number = np.random.randint(l, r)
+        if number == predict_number:
+            break  # выход из цикла если угадали
+        else:
+            if number > predict_number:
+                l = predict_number
+            elif number < predict_number:
+                r = predict_number
+    return count
+
+
+def score_game(random_predict) -> int:
+    """За какое количство попыток в среднем за 1000 подходов угадывает наш алгоритм
+
+    Args:
+        random_predict ([type]): функция угадывания
+
+    Returns:
+        int: среднее количество попыток
+    """
+    count_ls = []
+    np.random.seed(1)  # фиксируем сид для воспроизводимости
+    random_array = np.random.randint(1, 101, size=(1000))  # загадали список чисел
+
+    for number in random_array:
+        count_ls.append(random_predict(number))
+
+    score = int(np.mean(count_ls))
+    print(f"Ваш алгоритм угадывает число в среднем за:{score} попыток")
+    return score
+
+
+if __name__ == "__main__":
+    # RUN
+    score_game(random_predict)
